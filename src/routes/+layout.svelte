05626ea8ab6745bbb5a17eee07e79acce6cc245a
Matthew Godwin <matthew_bat@outlook.com>
2024-06-28 15:28:00 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+</script>
+<div class="bg-gradient-to-r from-purple-500 to-purple-900 h-screen w-screen">
+    <nav class="p-6">
+        <h1 class="text-xl">Matt Godwin</h1>
+    </nav>
+    
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="h-screen w-screen">
+    <ul class="absolute bottom-0 left-0 p-6">
+        <li>
+            <a href="https://x.com/enmattg" target="_blank">[x]</a>
+        </li>
+        <li>
+            <a href="https://github.com/MattGodwin" target="_blank">[github]</a>
+        </li>
+    </ul>
+</div>
