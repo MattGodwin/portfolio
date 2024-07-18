c8ca1379ade42e56288e1d451a9d40fed37a200d
Matthew Godwin <matthew_bat@outlook.com>
2024-07-18 15:30:37 +0100

    SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
+<script>
+	import Seo from 'sk-seo';
+</script>
+
+<Seo>
+	title="Matt Godwin Portfolio" description="Matt Godwin's Portfolio" keywords="Matt Godwin, Matthew
+	Godwin, Matt, Godwin" index="true"
+</Seo>
+
 <ul class="absolute bottom-0 left-0 p-6">
-    <li>
-        <a href="https://x.com/enmattg" target="_blank" class="hover:text-purple-100 ">[x]</a>
-    </li>
-    <li>
-        <a href="https://github.com/MattGodwin" target="_blank" class=" hover:text-purple-100">[github]</a>
-    </li>
+	<li>
+		<a href="https://x.com/enmattg" target="_blank" class="hover:text-purple-100">[x]</a>
+	</li>
+	<li>
+		<a href="https://github.com/MattGodwin" target="_blank" class=" hover:text-purple-100"
+			>[github]</a
+		>
+	</li>
 </ul>
